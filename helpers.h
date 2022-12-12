6f5171b1e4071c049cb1b336933d792059ce96d7
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-12 19:53:49 +0200

    Add readNode

--- helpers.h
+++ helpers.h
@@ -2,6 +2,7 @@
 #define ASSIGNMENT_2_HELPERS_H
 
 #include <fstream>
+#include <vector>
 
 //  Get the size of a file
 std::streamoff getFileSize(std::fstream &btree);
@@ -14,7 +15,7 @@ bool isEmpty(int recordIndex, int recordSize, std::fstream &btree);
 
 //  Reads the value in the second cell of the B-tree's header
 //  i.e. the index of the next empty record
-int getIndexOfNextEmptyRecord();
+int getIndexOfNextEmptyRecord(std::fstream &btree);
 
 //  Opens B-tree file named filename
 void openBTree(char *filename, std::fstream &btree);
@@ -24,5 +25,8 @@ void closeBTree(std::fstream &btree);
 
 //  Pads a string s with spaces till it's size becomes n
 std::string pad(const std::string &s, int n);
+
+//  Read the record at nodeIndex and return it as vector of pairs
+std::vector<std::pair<int, int>> readNode(int nodeIndex, int recordSize, std::fstream &btree);
 
 #endif //ASSIGNMENT_2_HELPERS_H
