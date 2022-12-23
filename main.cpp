b4bc1b21b9c215ac17f35707a5f7d73b640714ba
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-24 00:56:30 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
-#include <iostream>
 #include "BTree.h"
 
-void assignmentTestCase(BTree &btree);
+void insertions(BTree &btree);
 
 int main() {
     BTree btree("../btree", 10, 5, 5);
-
+    insertions(btree);
     return 0;
 }
 
-void assignmentTestCase(BTree &btree) {
+void insertions(BTree &btree) {
     btree.insert(3, 12);
     btree.insert(7, 24);
     btree.insert(10, 48);
     btree.insert(24, 60);
     btree.insert(14, 72);
-    
+
     btree.display();
-    
+
     btree.insert(19, 84);
+
+    btree.display();
+
     btree.insert(30, 96);
     btree.insert(15, 108);
     btree.insert(1, 120);
     btree.insert(5, 132);
+
+    btree.display();
+
     btree.insert(2, 144);
+
+    btree.display();
+
     btree.insert(8, 156);
     btree.insert(9, 168);
     btree.insert(6, 180);
@@ -31,6 +39,9 @@ void assignmentTestCase(BTree &btree) {
     btree.insert(12, 204);
     btree.insert(17, 216);
     btree.insert(18, 228);
+
+    btree.display();
+
     btree.insert(32, 240);
 
     btree.display();
